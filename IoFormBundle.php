9382272818a372a8457660e349637752f4c7b296
Alessio <io.alessio@gmail.com>
2011-09-25 00:13:27 +0200

    License

--- IoFormBundle.php
+++ IoFormBundle.php
@@ -1,5 +1,15 @@
 <?php
 
+
+/*
+ * This file is part of the IoFormBundle package
+ *
+ * (c) Alessio Baglio <io.alessio@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Io\FormBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
