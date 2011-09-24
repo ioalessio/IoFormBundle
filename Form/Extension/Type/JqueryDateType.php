e05bd591817ad7433b7a8f294ba4ef0ffe377371
Alessio <io.alessio@gmail.com>
2011-09-24 02:28:31 +0200

    fix jquery_date - added michelpa's features

--- Form/Extension/Type/JqueryDateType.php
+++ Form/Extension/Type/JqueryDateType.php
@@ -9,17 +9,37 @@ namespace Io\FormBundle\Form\Extension\Type;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormBuilder;
 
 class JqueryDateType extends DateType
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilder $builder, array $options) {
 
-    public function getDefaultOptions(array $options)
-    {
-      $options = parent::getDefaultOptions($options);
-      //Works only with single text
-      $options['widget'] = 'single_text';
+        $changemonth = $options['changeMonth'];
+        $changeyear = $options['changeYear'];
+        $mindate = $options['minDate'];
+        $maxdate = $options['maxDate'];
 
-      return $options;
+        $builder->setAttribute('changemonth', $changemonth);
+        $builder->setAttribute('changeyear', $changeyear);
+        $builder->setAttribute('mindate', $mindate);
+        $builder->setAttribute('maxdate', $maxdate);
+
+        parent::buildForm($builder, $options);
+    }
+
+    public function getDefaultOptions(array $options) {
+        $options = parent::getDefaultOptions($options);
+        //Works only with single text
+        $options['widget'] = 'single_text';
+        $options['changeMonth'] = 'false';
+        $options['changeYear'] = 'false';
+        $options['minDate'] = null;
+        $options['maxDate'] = null;
+        return $options;
     }
 
    /**
@@ -36,11 +56,13 @@ class JqueryDateType extends DateType
     public function buildViewBottomUp(FormView $view, FormInterface $form)
     {
         $view->set('widget', $form->getAttribute('widget'));
+
         $pattern = $form->getAttribute('formatter')->getPattern();
         $format = $pattern;
+
         if ($view->hasChildren()) {
 
-        // set right order with respect to locale (e.g.: de_DE=dd.MM.yy; en_US=M/d/yy)
+            // set right order with respect to locale (e.g.: de_DE=dd.MM.yy; en_US=M/d/yy)
             // lookup various formats at http://userguide.icu-project.org/formatparse/datetime
             if (preg_match('/^([yMd]+).+([yMd]+).+([yMd]+)$/', $pattern)) {
                 $pattern = preg_replace(array('/y+/', '/M+/', '/d+/'), array('{{ year }}', '{{ month }}', '{{ day }}'), $pattern);
@@ -50,13 +72,33 @@ class JqueryDateType extends DateType
             }
         }
 
+        $view->set('date_pattern', $pattern);
+        $view->set('date_format', $this->convertJqueryDate($pattern));
+        $view->set('change_month', $form->getAttribute('changemonth'));
+        $view->set('change_year', $form->getAttribute('changeyear'));
+        $view->set('min_date', $form->getAttribute('mindate'));
+        $view->set('max_date', $form->getAttribute('maxdate'));
+    }
+
+    protected function convertJqueryDate($pattern)
+    {
+      $format = $pattern;
         //jquery use a different syntax, have to replace
+        //  php    jquery
+        //  MM      mm
+        //  MMM     M
+        //  MMMM    MM
+        //  y       yy
+
+        if (strpos($format, "MMM") > 0) {
+            $format = str_replace("MMM", "M", $format);
+        } else {
+            $format = str_replace("MM", "mm", $format);
+        }
         $format = str_replace("LLL", "M", $format);
         $format = str_replace("y", "yy", $format);
 
-
-        $view->set('date_pattern', $pattern);
-        $view->set('date_format', $format);
+       return $format;
     }
 
 }
