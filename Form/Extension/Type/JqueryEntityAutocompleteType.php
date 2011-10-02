c0b44f96468d0a2215b5cd20b7138eb6ee8fd47d
Pascal <michelpa@gmail.com>
2011-10-02 18:32:24 +0200

    ajout d'un autocomplete basé sur une entite

--- Form/Extension/Type/JqueryEntityAutocompleteType.php
+++ Form/Extension/Type/JqueryEntityAutocompleteType.php
@@ -3,11 +3,16 @@
 namespace Io\FormBundle\Form\Extension\Type;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\Exception\FormException;
+
 
 /**
  * Render a Jquery autocomplete input to populate a  Select of an entity
  */
-class JqueryEntityComboboxType extends EntityType {
+class JqueryEntityAutocompleteType extends EntityType {
 
     public function getName() {
         return "jquery_entity_autocomplete";
@@ -16,13 +21,15 @@ class JqueryEntityComboboxType extends EntityType {
     public function getDefaultOptions(array $options) {
         $options = parent::getDefaultOptions($options);
         $options['source'] = '';
-        $options['minlength'] = 0;
+        $options['minlength'] = 1;
 
         //DEFAULT VALUE = NULL
         $options['empty_value'] = "";
         return $options;
     }
 
+    
+    
     /**
      * {@inheritdoc}
      */
@@ -33,7 +40,20 @@ class JqueryEntityComboboxType extends EntityType {
         $builder->setAttribute('source', $source);
         $builder->setAttribute('minlength', $minlength);
 
+        if ('' == $source) {
+            throw new FormException(sprintf('The option "%s" can not be empty and must be a valid route', 'source'));
+        }
+        
         parent::buildForm($builder, $options);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildViewBottomUp(FormView $view, FormInterface $form) {
+
+        $view->set('source', $form->getAttribute('source'));
+        $view->set('minlength', $form->getAttribute('minlength'));
     }
 
 }
