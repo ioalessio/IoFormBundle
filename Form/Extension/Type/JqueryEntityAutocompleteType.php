b65b746d9efab13827b4f172efe0b9154168877c
Seb Armand <khepin@gmail.com>
2011-10-14 22:31:50 +0800

    add entity autocomplete field

---
+++ Form/Extension/Type/JqueryEntityAutocompleteType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Io\FormBundle\Form\Extension\Type;
+
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+class JqueryEntityAutocompleteType extends EntityIdType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder->setAttribute('url', $options['url']);
+        $builder->setAttribute('callback', $options['callback']);
+        $builder->add('test', 'text');
+
+        parent::buildForm($builder, $options);
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+        $options = parent::getDefaultOptions($options);
+        $options['multiple'] = false;
+        // jQuery will pass a single "term" variable. This url should include '$$term$$' that will be replaced while querying
+        $options['url'] = false;
+        $options['callback'] = false;
+
+        //DEFAULT VALUE = NULL
+        $options['empty_value'] = "";
+        return $options;
+    }
+
+    public function getName()
+    {
+        return 'jquery_entity_autocomplete';
+    }
+
+    public function buildViewBottomUp(FormView $view, FormInterface $form)
+    {
+        parent::buildViewBottomUp($view, $form);
+        $view->set('url', $form->getAttribute('url'));
+        $view->set('callback', $form->getAttribute('callback'));
+    }
+}
